9e92302fa232e8662904f1f265ad4b9a3f8b940f
Esther Nahm <efnahm@gmail.com>
2019-07-15 11:54:49 -0400

    formatted

--- java/src/main/java/com/techelevator/npgeek/parkController.java
+++ java/src/main/java/com/techelevator/npgeek/parkController.java
@@ -29,68 +29,64 @@ public class parkController {
 	@Autowired
 	private SurveyDAO surveyDAO;
 
-	@RequestMapping(path= {"/", "home"}, method=RequestMethod.GET)
+	@RequestMapping(path = { "/", "home" }, method = RequestMethod.GET)
 	public String showHomePage(ModelMap model) {
 		model.addAttribute("allParks", parkDAO.getParks());
 		return "homePage";
-		}
-	
-	@RequestMapping(path="/parkDetail", method=RequestMethod.GET)
+	}
+
+	@RequestMapping(path = "/parkDetail", method = RequestMethod.GET)
 	public String showParkDetail(ModelMap model, @RequestParam String parkCode, HttpSession session) {
 		Park park = parkDAO.getParkByCode(parkCode);
 		model.put("park", park);
-		
-		parkCode = parkCode.endsWith(",") ? parkCode.substring(0, parkCode.length()-1) : parkCode;
+
+		parkCode = parkCode.endsWith(",") ? parkCode.substring(0, parkCode.length() - 1) : parkCode;
 		List<Weather> weather = weatherDAO.getWeatherByCode(parkCode);
 		model.put("weather", weather);
-		
-		String tempUnit = (String)session.getAttribute("tempUnit");
-		if(tempUnit==null){
-			tempUnit="F";
+
+		String tempUnit = (String) session.getAttribute("tempUnit");
+		if (tempUnit == null) {
+			tempUnit = "F";
 			session.setAttribute("tempUnit", tempUnit);
 		}
 		model.put("tempUnit", tempUnit);
-		
-		
 		return "selectedParkDetail";
 	}
-	
-	
-	
-	@RequestMapping(path="/weatherDetail", method=RequestMethod.POST)
-	public String showSelectedPark(ModelMap model, @RequestParam String parkCode, @RequestParam String tempUnit, HttpSession session) {
-		parkCode = parkCode.endsWith(",") ? parkCode.substring(0, parkCode.length()-1) : parkCode;
+
+	@RequestMapping(path = "/weatherDetail", method = RequestMethod.POST)
+	public String showSelectedPark(ModelMap model, @RequestParam String parkCode, @RequestParam String tempUnit,
+			HttpSession session) {
+		// to hash out the added , at the end of the parkCode
+		parkCode = parkCode.endsWith(",") ? parkCode.substring(0, parkCode.length() - 1) : parkCode;
 
 		session.setAttribute("tempUnit", tempUnit);
 
 		return "redirect:/parkDetail?parkCode=" + parkCode;
-	}	
-	
-	@RequestMapping(path="/userInput", method=RequestMethod.GET)
+	}
+
+	@RequestMapping(path = "/userInput", method = RequestMethod.GET)
 	public String userSurveyInput(ModelMap model) {
 		model.addAttribute("allParks", parkDAO.getParks());
 		return "userInput";
 	}
-	
-	@RequestMapping(path="/userInput", method=RequestMethod.POST)
-	public String processUserSurveyInput(HttpSession session, @RequestParam String emailAddress, @RequestParam String state, @RequestParam String activityLevel, @RequestParam String parkCode) {
-		 surveyDAO.saveSurveyResults(parkCode, emailAddress, state, activityLevel); 
-		 return "redirect:/surveyResult";
+
+	@RequestMapping(path = "/userInput", method = RequestMethod.POST)
+	public String processUserSurveyInput(HttpSession session, @RequestParam String emailAddress,
+			@RequestParam String state, @RequestParam String activityLevel, @RequestParam String parkCode) {
+		surveyDAO.saveSurveyResults(parkCode, emailAddress, state, activityLevel);
+		return "redirect:/surveyResult";
 	}
-	
-	
-	@RequestMapping(path="/surveyResult", method=RequestMethod.GET)
+
+	@RequestMapping(path = "/surveyResult", method = RequestMethod.GET)
 	public String showSurveyResult(ModelMap model) {
 		List<FavoriteParks> favoriteparks = surveyDAO.favoriteParks();
-		for (FavoriteParks fp: favoriteparks) {
+		for (FavoriteParks fp : favoriteparks) {
 			Park park = parkDAO.getParkByCode(fp.getParkCode());
 			fp.setPark(park);
 		}
 		model.addAttribute("favoriteParks", favoriteparks);
-		
+
 		return "surveyResult";
-		}
-	
-	
-	
+	}
+
 }
